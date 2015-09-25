6e54d658335bd9bd02009964f5257258f43575d4
Jan Ivar Beddari <code@beddari.net>
2015-09-25 15:11:55 +0200

    Initial commit ...wip

---
+++ manifests/config.pp
@@ -0,0 +1,24 @@
+# == Class named_interfaces::config
+#
+# Sets up an external structured fact $named_interfaces
+#
+class named_interfaces::config {
+
+  unless empty($named_interfaces::interfaces) {
+
+    if $named_interfaces::manage_facterdirs {
+      file { [$named_interfaces::facter_root,
+              $named_interfaces::facts_d]:
+        ensure => directory,
+      }
+      File[$named_interfaces::facts_d] -> Hash_file[$named_interfaces::fact_file]
+    }
+
+    hash_file { $named_interfaces::fact_file:
+      value    => { 'named_interfaces' => $named_interfaces::interfaces },
+      provider => 'yaml',
+    }
+
+  }
+
+}
---
+++ manifests/init.pp
@@ -0,0 +1,59 @@
+# == Class: named_interfaces
+#
+# This module creates a YAML file /etc/facter/facts.d/named_interfaces.yaml
+# to provide a custom fact $named_interfaces
+#
+# The module ships with additional code that creates additional facts based on
+# the named interfaces you configure. This allows you to use abstract interface
+# names in Puppet code and Hiera data so that e.g switching interfaces or IP
+# addresses are easier.
+#
+# === Parameters
+#
+# [*config*]
+#   Config hash specifying a named_interfaces configuration, e.g
+#
+#     {"mgmt"=>["eth1"], "transport"=>["eth2", "eth3"]}
+#
+#   This configuration will make additional network facts available, e.g
+#
+#   ipaddress_mgmt1 will resolve to ipaddress_eth1
+#   macaddress_mgmt1 will resolve to macaddress_eth1
+#   netmask_transport2 will resolve to netmask_eth3
+#
+# [*facter_root*]
+#   Facter configuration folder. Defaults to /etc/facter
+#
+# [*facts_d*]
+#   Full path to the facts.d folder, normally inside facter_root. Defaults to
+#   /etc/facter/facts.d
+#
+# [*fact_file*]
+#   Full path of the file that will contain the YAML-format $named_interfaces
+#   fact. Defaults to /etc/facter/facts.d/named_interfaces.yaml
+#
+# [*manage_factdirs*]
+#   Manage and create facter_root and facts_d directories if they don't exist
+#   Defaults to true.
+#
+class named_interfaces (
+  $config            = undef,
+  $facter_root       = '/etc/facter',
+  $facts_d           = '/etc/facter/facts.d',
+  $fact_file         = '/etc/facter/facts.d/named_interfaces.yaml',
+  $manage_facterdirs = true,
+) {
+  validate_bool($manage_facterdirs)
+
+  # Relying on hiera to return undef requires Puppet 3.8 or later
+  $interfaces = $config ? {
+    undef   => hiera_hash('named_interfaces', undef),
+    default => $config,
+  }
+
+  # validate data
+  validate_hash($interfaces)
+
+  contain 'named_interfaces::config'
+}
+
