63188730d50d26497bae286b47e6eae210237c51
Jan Ivar Beddari <code@beddari.net>
2015-09-27 18:42:18 +0200

    Use return value from Facter.value
    If false, do not create any new facts

--- lib/facter/named_interfaces.rb
+++ lib/facter/named_interfaces.rb
@@ -1,19 +1,25 @@
-# read hash from named_interfaces fact
-hash = Facter.value(:named_interfaces)
+# read value of named_interfaces fact
+if Facter.value(:named_interfaces)
 
-# set up interface aliases for these facts
-facts = %w(ipaddress macaddress mtu netmask network)
+  hash = Facter.value(:named_interfaces)
+  hash.each_pair do |key,array|
+    hash[key].each.with_index(1) do |iface, index|
 
-hash.each_pair do |key,array|
-  hash[key].each.with_index(1) do |iface, index|
-    facts.each do |fact|
-      if Facter.value("#{fact}_#{iface}")
-        Facter.add("#{fact}_#{key}#{index}") do
-          setcode do
-            Facter.value("#{fact}_#{iface}")
+      # set up interface aliases for these facts
+      facts = %w(ipaddress macaddress mtu netmask network)
+      facts.each do |fact|
+        if Facter.value("#{fact}_#{iface}")
+          # Add a new fact using key and index as interface
+          # The fact value is copied from the original interface
+          Facter.add("#{fact}_#{key}#{index}") do
+            setcode do
+              Facter.value("#{fact}_#{iface}")
+            end
           end
         end
       end
+
     end
   end
+
 end
