e7ab3e50f11c0d3da37e0b82ed614df9a024f78e
Jan Ivar Beddari <code@beddari.net>
2015-09-29 14:05:58 +0200

    Add interface_<name> fact
    Create a fact that back-references the original interface name

--- lib/facter/named_interfaces.rb
+++ lib/facter/named_interfaces.rb
@@ -4,6 +4,12 @@ if Facter.value(:named_interfaces)
 
   hash.each_pair do |key,array|
     hash[key].each.with_index(1) do |iface, index|
+      # create a fact that reference back to the original interface name
+      Facter.add("interface_#{key}#{index}") do
+        confine { Facter.value(:interfaces).split(',').include? iface }
+        setcode { iface }
+      end
+
       # set up interface aliases for these facts
       facts = %w(ipaddress macaddress mtu netmask network)
 
